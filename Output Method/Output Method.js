3c790624a811c95d4127e97ea6a006418e10930a
Vivek Prajapati <vivekprajapati0405@gmail.com>
2025-01-07 16:53:45 +0530

    03/12 Output Method

---
+++ Output Method/Output Method.js
@@ -0,0 +1,38 @@
+// 1. Console:
+
+// console.log();
+console.log("Hello, World!");
+
+
+
+// console.table();
+console.table([1, 2, 3]);
+// output
+
+
+// console.error();
+console.error("Something went wrong!");
+
+
+// console.warn();
+console.warn("This is a warning!");
+
+
+
+
+// 2. Browser Window:
+// alert(); 
+alert("This is an alert!");
+
+
+// document.write(); 
+document.write("Hello, World!"); 
+
+
+// 3. HTML Elements:
+
+// innerHTML
+document.getElementById("name").innerHTML = "Vivek Prajapati!";
+
+// innerText
+document.getElementById("profession").textContent = "Full Stack Developer!"; 
